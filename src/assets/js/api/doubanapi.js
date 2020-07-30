923ee2b7fc88d36ed6b09c441ed3b9e93e6e2142
Criska <619729921@qq.com>
2020-07-30 17:47:55 +0800

    电影找电影div和详情页的处理

--- src/assets/js/api/doubanapi.js
+++ src/assets/js/api/doubanapi.js
@@ -3,17 +3,17 @@ import axios from 'axios'
 //api请求的封装
 axios.defaults.headers.post['Content-Type'] = 'application/json';
 //axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded;charset=utf-8';
-var API_DB_URL= "/doubanapi"
-var API_Music_URL= ""
-function request(url, params, method,type) {
-var dataurl = null
-  if(type==="douban"){
-	  dataurl = API_DB_URL + url
-  }else{
-	  dataurl = API_Music_URL + url
+var API_DB_URL = "/doubanapi"
+var API_Music_URL = ""
+function request(url, params, method, type) {
+  var dataurl = null
+  if (type === "douban") {
+    dataurl = API_DB_URL + url
+  } else {
+    dataurl = API_Music_URL + url
   }
   //var dataurl = `${API_BASE_URL}${url}`
- 
+
   if (method === 'get') {
     return axios.get(dataurl, {
       params: params
@@ -26,7 +26,7 @@ var dataurl = null
     // 发送 POST 请求
     return axios({
       method: 'post',
-      headers:{"token": tokenzn},
+      headers: { "token": tokenzn },
       url: dataurl,
       data: params
     }).then(function (res) {
@@ -38,23 +38,31 @@ var dataurl = null
 var DBAPI = {
   //获取豆瓣热门电影
   getHotMovie: function (data, method) {
-    return request('/movie/in_theaters', data, method,'douban')
+    return request('/movie/in_theaters', data, method, 'douban')
   },
   //获取电影一周口碑榜
   getWeeklyMoive: function (data, method) {
-    return request('/movie/weekly', data, method,'douban')
+    return request('/movie/weekly', data, method, 'douban')
   },
   //获取电影一周口碑榜
   getTopMoive: function (data, method) {
-    return request('/movie/top250', data, method,'douban')
+    return request('/movie/top250', data, method, 'douban')
   },
   //获取新片榜
-  getNewmovies:function (data, method) {
-    return request('/movie/new_movies', data, method,'douban')
+  getNewmovies: function (data, method) {
+    return request('/movie/new_movies', data, method, 'douban')
   },
   //即将上映的电影
-  getComingMovie:function (data, method) {
-    return request('/movie/coming_soon', data, method,'douban')
+  getComingMovie: function (data, method) {
+    return request('/movie/coming_soon', data, method, 'douban')
+  },
+  //获取电影剧照
+  getMovieimg: function (data, id, method) {
+    return request(`/movie/subject/${id}/photos`, data, method, 'douban')
+  },
+  //获取电影详情
+  getMoviemore: function (data, id, method) {
+    return request(`/movie/subject/${id}`, data, method, 'douban')
   }
 }
 export default DBAPI
